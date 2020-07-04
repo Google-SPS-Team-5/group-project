b4d2009603c1052d1ea67057a2c36a11f3945de0
Jacob <lipech@hotmail.com>
2020-07-04 15:49:35 +0800

    replace locations with an array

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,23 +1,23 @@
 function initMap() {
-  var foodLocations = { 'mozerella': {lat: 1.326574, lng: 103.814158},
-                        'pastries': {lat: 1.322040, lng: 103.814554},
-                        'curry puffs': {lat: 1.323561, lng: 103.841931} };
+  var foodLocations = [{'position': {lat: 1.326574, lng: 103.814158}, 'desc': 'Mozerella cheeseball' },
+                       {'position': {lat: 1.322040, lng: 103.814554}, 'desc': 'Pastries' },
+                       {'position': {lat: 1.323561, lng: 103.841931}, 'desc': 'Curry puffs'}];
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 1.3521, lng: 103.8198 },
     zoom: 14
   });
 
   var marker;
-  for (const [key, value] of Object.entries(foodLocations)) {
-    marker = new google.maps.Marker({position: value, map: map});
-    (function(marker, key) {
+  for (i in foodLocations) {
+    marker = new google.maps.Marker({position: foodLocations[i].position, map: map});
+    (function(marker, i) {
       // add click event
       google.maps.event.addListener(marker, 'click', function() {
         infowindow = new google.maps.InfoWindow({
-            content: key
+            content: foodLocations[i].desc
         });
         infowindow.open(map, marker);
       });
-    })(marker, key);
+    })(marker, i);
   }
 }
