73f6976fa8b4a4c7a7167a94bad64d5f94266861
g-lilian <guolilian98@gmail.com>
2020-07-07 17:23:11 +0800

    Remove listings tab in navbar, add navbar to product.html

--- src/main/webapp/navbar.js
+++ src/main/webapp/navbar.js
@@ -1,16 +1,11 @@
 document.write('\
 <div id="nav-container">\
-    <a href="index.html" class="nav-link"><strong>Home</strong></a>\
-    <a href="listings.html" class="nav-link"><strong>Browse listings</strong></a>\
-    <a href="about.html" class="nav-link"><strong>About us</a>\
+    <a href="index.html" class="nav-link">Home</a>\
+    <a href="about.html" class="nav-link">About us</a>\
     <div id="search-container">\
         <form action="/placeholder.js">\
-            <div id="nav-bar-search-container">\
-              <input type="text" placeholder="Search for food or snacks">\
-              <button type="submit"\
-                <i class="fa fa-search"></i>\
-              </button>\
-            </div>\
+            <input type="text" placeholder="Search listing...">\
+            <button type="submit"><i class="fa fa-search"></i></button>\
         </form>\
     </div>\
 </div>'
