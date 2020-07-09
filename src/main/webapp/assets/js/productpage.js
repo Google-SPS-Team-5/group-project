ae116a3b418701c2b47275a0f21a59fda17ff480
Jireh Chew <jireh_cwe@hotmail.com>
2020-07-10 00:40:47 +0800

    Populate thumbnail images with JS and remove hardcoded data.

--- src/main/webapp/assets/js/productpage.js
+++ src/main/webapp/assets/js/productpage.js
@@ -15,13 +15,24 @@ async function populateProductDetails() {
 
 function populateBusinessDescription(business) {
   Array.from(document.getElementsByClassName('businessName')).forEach(element => element.innerHTML += business.name);
+
   document.getElementById("businessLogo").src = business.logoBlobstoreUrl;
+
   const categoryContainer = document.getElementById("businessCategories");
   business.categories.forEach(category => categoryContainer.appendChild(createCategoryElement(category)));
+
+  document.getElementById("businessUrl").href = business.websiteUrl;
+
+  const ratingContainer = document.getElementById("aggregateRating");
+  //TODO: Fix star CSS and use CSS to set float rating
 }
 
 function populateImageGallery(business) {
-  
+  const imagePreviewGallery = document.getElementById("image-thumbnail-gallery");
+  const modalGallery = document.getElementById("myModal");
+
+  business.photoBlobstoreUrlList.forEach(photoUrl => imagePreviewGallery.innerHTML += createThumbnailImageElement(photoUrl));
+
 }
 
 function populateBusinessWriteup(business) {
@@ -36,4 +47,22 @@ function createCategoryElement(categoryName) {
   const category = document.createElement("mark");
   category.innerHTML = categoryName;
   return category;
+}
+
+function createThumbnailImageElement(imageUrl) {
+  return `<div class="lightbox-column">
+          <img
+            src=${imageUrl}
+            style="height: 250px; width:auto"
+            onclick="openModal();currentSlide(1)"
+            class="hover-shadow cursor"
+          />`;
+}
+
+function createGalleryImageElement(imageUrl) {
+
+}
+
+function createGalleryImagePreviewElement(imageUrl) {
+
 }
