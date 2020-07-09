ff7a328c16a585cf705e0962070e70b64e1061ed
Dalis Chan <dalischan97@gmail.com>
2020-07-09 22:05:08 +0800

    Add footer

---
+++ src/main/webapp/assets/js/footer.js
@@ -0,0 +1,13 @@
+document.write('\
+<div id="footer">\
+  <div>\
+  <h2>Placeholder</h2>\
+  </div>\
+  <div>\
+    <p>Contact Us</p>\
+    <p>General enquiries: test@testmail.com</p>\
+    <p>Interested in listing your business?</p>\
+    <p>Let us know here!</p>\
+  </div>\
+</div>'
+);
