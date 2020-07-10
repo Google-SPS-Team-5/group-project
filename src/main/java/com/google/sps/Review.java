7f7c91d3e3aa5898cbccde6ecf2e1f80350fd9dd
g-lilian <guolilian98@gmail.com>
2020-07-10 13:07:57 +0800

    Add review class

--- src/main/java/com/google/sps/Review.java
+++ src/main/java/com/google/sps/Review.java
@@ -1,12 +1,28 @@
+package com.google.sps;
 
+import java.time.LocalDate;
 
+/**
+ * Holds all relevant data to a business review on the site.
+ */
 public final class Review {
-    private String userID; // Datastore key of user the review belongs to
-    private String comment;
-    private int rating;
-    private DateTime time;
-    
-    public Review(
+  private String userID; // Datastore key for the user that this review belongs to
+  private String comment;
+  private int rating;
+  private LocalDate dateTime;
 
-    )
+  public Review(String userID,
+                String comment,
+                int rating,
+                LocalDate dateTime) {
+    this.userID = userID;
+    this.comment = comment;
+    this.rating = rating;
+    this.dateTime = dateTime;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Comment: %s, Rating: %d", this.comment, this.rating);
+  }
 }
