95c603558bb21254c50460a6118ab361edf11c96
g-lilian <guolilian98@gmail.com>
2020-07-10 12:41:12 +0800

    Publish branch for reviews

---
+++ src/main/java/com/google/sps/Review.java
@@ -0,0 +1,12 @@
+
+
+public final class Review {
+    private String userID; // Datastore key of user the review belongs to
+    private String comment;
+    private int rating;
+    private DateTime time;
+    
+    public Review(
+
+    )
+}
