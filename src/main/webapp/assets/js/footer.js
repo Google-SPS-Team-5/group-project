f7657794a52acfd44a7407427a4838a67be8196f
Dalis Chan <dalischan97@gmail.com>
2020-07-21 17:07:34 +0800

    Edit margins

--- src/main/webapp/assets/js/footer.js
+++ src/main/webapp/assets/js/footer.js
@@ -1,11 +1,11 @@
 document.write('\
 <div id="footer">\
   <div>\
-  <h1>TuckShop</h1>\
+  <h2>TuckShop</h2>\
   </div>\
   <div>\
-    <h2>Contact Us</h2>\
-    <h3>Interested in listing your business?</h3>\
+    <h3>Contact Us</h3>\
+    <h4>Interested in listing your business?</h4>\
     <p>Let us know <a id="g-form" href="https://docs.google.com/forms/d/e/1FAIpQLSdq8M3cse7ak1NDdrnZ0nfUz9PCXepse-g-PyKRlRatpAKMtw/viewform?usp=sf_link" target="_blank">here</a>!</p>\
   </div>\
 </div>'
